51fad2b350e1faa9ad8419ff60417df84336b00a
Economicsnerd <idrissamraoui@protonmail.com>
2020-11-30 10:06:21 -0500

    Added Styling and Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    imageUrl: null,
+    imageAlt: null,
+  };
+
+  render() {
+    const { imageUrl, imageAlt } = this.state;
+
+    return (
+      <main className="App">
+        <section className="left-side">
+          <form>
+            <div className="form-group">
+              <input type="file" />
+            </div>
+
+            <button
+              type="button"
+              className="btn"
+              onClick={this.handleImageUpload}
+            >
+              Submit
+            </button>
+            <button type="button" className="btn widget-btn">
+              Upload Via Widget
+            </button>
+          </form>
+        </section>
+        <section className="right-side">
+          <p>The resulting image will be displayed here</p>
+          {imageUrl && (
+            <img src={imageUrl} alt={imageAlt} className="displayed-image" />
+          )}
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
